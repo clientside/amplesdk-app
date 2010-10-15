710d8b439d22ae41d3fd7aa2d9bb197289027ee0
ilinsky <sergey@ilinsky.com>
2010-10-15 16:56:28 +0200

    Fixed authenticated user initial screen

--- src/commands/_ReadyCommand.js
+++ src/commands/_ReadyCommand.js
@@ -7,6 +7,7 @@ Main._ReadyCommand.prototype	= new Command;
 Main._ReadyCommand.prototype.execute	= function() {
 	//
 	if (this.controller.model.getProxy("UserProxy").getData("key") == "secrets") {
+		this.controller.sendNotification("HideLogin");
 		this.controller.sendNotification("ShowWorkspace");
 	}
 	else {
