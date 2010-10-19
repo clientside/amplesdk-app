c4a5141aa887133e67b66d6f094452f5d3fe994c
ilinsky <sergey@ilinsky.com>
2010-10-19 12:16:09 +0200

    Added getMediator method

--- mvc/classes/View.js
+++ mvc/classes/View.js
@@ -13,6 +13,10 @@ View.prototype.init	= function() {
 };
 
 //
+View.prototype.getMediator	= function(sName) {
+	return this.mediators.hasOwnProperty(sName) ? this.mediators[sName] : null;
+};
+
 View.prototype.addMediator	= function(sName, oMediator) {
 	if (!(oMediator instanceof Mediator))
 		throw String(oMediator) + " is not instanceof Mediator";
