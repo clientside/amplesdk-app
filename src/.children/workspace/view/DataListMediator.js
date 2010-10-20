b1d61e90368de293b5210f5de31cb89471b5e029
ilinsky <sergey@ilinsky.com>
2010-10-20 18:17:48 +0200

    Minor

--- src/.children/workspace/view/DataListMediator.js
+++ src/.children/workspace/view/DataListMediator.js
@@ -6,6 +6,18 @@ Workspace.DataListMediator.prototype	= new MVC.Mediator;
 
 Workspace.DataListMediator.prototype.onRegister	= function() {
 	ample.query(this.element).bind("click", this);
+
+	// Populate list
+	var dataEntries	= this.facade.retrieveProxy("DataProxy").data,
+		listbody	= this.element.body;
+	for (var nIndex = 0; nIndex < dataEntries.length; nIndex++) {
+		ample.query(listbody).append(
+				ample.query("<xul:listitem>\
+								<xul:listcell>" + dataEntries[nIndex].id + "</xul:listcell>\
+								<xul:listcell>" + dataEntries[nIndex].name + "</xul:listcell>\
+								<xul:listcell>" + dataEntries[nIndex].description + "</xul:listcell>\
+							</xul:listitem>"));
+	}
 };
 
 Workspace.DataListMediator.prototype.onRemove		= function() {
--- srv/auth.php
+++ srv/auth.php
@@ -1,5 +1,5 @@
 <?php
-sleep(2);
+//sleep(2);
 	switch ($_GET["action"]) {
 		case "login":
 //			if (isset($_POST["login"]) && $_POST["login"] == "admin" && isset($_POST["password"]) && $_POST["password"] == "admin")
