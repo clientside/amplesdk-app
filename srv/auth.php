0a0732b4d4bbcabe361cd72359826fc1bd5815fa
ilinsky <sergey@ilinsky.com>
2010-10-20 17:35:20 +0200

    Sample server-side script

---
+++ srv/auth.php
@@ -0,0 +1,15 @@
+<?php
+sleep(2);
+	switch ($_GET["action"]) {
+		case "login":
+//			if (isset($_POST["login"]) && $_POST["login"] == "admin" && isset($_POST["password"]) && $_POST["password"] == "admin")
+				echo "a23hhje33h3912p";
+//			else
+//				echo "failure";
+			break;
+
+		case "logout":
+			echo "success";
+			break;
+	}
+?>
